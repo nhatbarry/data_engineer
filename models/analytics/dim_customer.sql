68e56821406be2eb363573294bdc1741abed199d
nhatbarry <nhatbarry@gmail.com>
2024-08-07 21:47:11 +0700

    0106 half

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,4 @@
 SELECT 
-  *
+  cast(customer_id as integer) as customer_key
+  , cast(customer_name as string) as customer_name
 FROM `vit-lam-data.wide_world_importers.sales__customers`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,8 @@
 SELECT 
-  order_line_id as sales_order_line_key
-  , quantity as quantity
-  , unit_price as unit_price
+  cast (order_line_id as integer) as sales_order_line_key
+  , cast(sales__orders.customer_key as integer) as customer_key
+  , cast (quantity as integer) as quantity
+  , cast (unit_price as numeric) as unit_price
+  , cast (quantity as integer) * cast (unit_price as numeric) as gross_amount
+  , cast (stock_item_id as integer) as product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
